82c6b8ad8fe4cfb25f88364916c2c1f331c4d0f3
A Torres <atorres@thecodeofarms.com>
2025-09-09 22:58:54 -0600

    Initialize project

---
+++ SyncedList/SyncedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncedListApp.swift
+//  SyncedList
+//
+//  Created by Armando Torres on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SyncedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
